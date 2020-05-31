04220a654c02353315b26290d14934573760506c
nischal08 <nischal.karanjit@gmail.com>
2020-05-31 20:02:17 +0545

    al completed

--- js/main.js
+++ js/main.js
@@ -18,8 +18,9 @@ window.addEventListener('scroll', function () {
   }
 });
 
-$('#navbar a,.btn').on('click', function (event) {
+$('#navbar a').on('click', function (event) {
   if (this.hash !== '');
+  event.preventDefault();
   const hash = this.hash;
   $('html,body').animate(
     {
